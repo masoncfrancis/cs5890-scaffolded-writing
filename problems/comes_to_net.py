80912137f4778bfa2948b0aed69a3f6554190231
Seth Poulsen <poulsenseth@yahoo.com>
2023-09-12 14:04:42 -0600

    add some more possible solutions to the tennis problem

--- problems/comes_to_net.py
+++ problems/comes_to_net.py
@@ -1,5 +1,7 @@
+###############################################################################
+#### DO NOT EDIT THIS SECTION
+###############################################################################
 from typing import Dict, Any, List, Tuple, Optional
-from typing import Dict, Any
 from shared_utils import set_weighted_score_data
 from scaffolded_writing.cfg import ScaffoldedWritingCFG
 from scaffolded_writing.student_submission import StudentSubmission
@@ -17,18 +19,29 @@ def grade(data: Dict[str, Any]) -> None:
 #### DO NOT EDIT ABOVE HERE, ONLY EDIT BELOW
 ###############################################################################
 
+statement = "Assume you are a right-handed tennis player.' + \
+    'A right-handed oppenent approaches the net with a forehand down the line. What is your next move?"
+
 PROBLEM_CONFIG = ScaffoldedWritingCFG.fromstring(f"""
     START -> "Hit a" SHOT_TYPE
+    SHOT -> SHOT_TYPE SHOT_LOCATION
     SHOT_TYPE -> "forehand" | "backhand"
+    SHOT_LOCATION -> "lob" | "down the line" | "cross-court" | EPSILON
+    EPSILON ->
 """)
 
 def grade_statement(tokens: List[str]) -> Tuple[bool, Optional[str]]:
     submission = StudentSubmission(tokens, PROBLEM_CONFIG)
 
-    if submission.does_path_exist("SHOT_TYPE", "backhand"):
-        return False, 'If you run around the ball to hit a forehand, you will be too far out of position if they volley to your forehand side.'
+    if submission.does_path_exist("SHOT_LOCATION", "cross-court"):
+        return False, 'If you hit cross-court, your oppenent can volley down the line' + \
+            ' on your forehand side faster than you can get there.'
+
+    if submission.does_path_exist("SHOT_TYPE", "forehand") and \
+        not submission.does_path_exist('SHOT_LOCATION', 'lob'):
+        return False, 'If you run around the ball to hit a forehand, ' + \
+            'you will be too far out of position if they volley to your forehand side.'
 
     return True, None
 
-statement = "Your oppenent approaches the net with a forehand down the line. What is your next move?"
 
